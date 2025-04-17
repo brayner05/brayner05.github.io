8a0b5a10c782edd8c94debdad466e46d4cfcf711
Brayden Rayner <braydenrayner@outlook.com>
2025-04-16 21:09:01 -0300

    Rewrite done (for now. I would like to tweak it a tad bit more, but it is ready to be hosted).

--- js/main.js
+++ js/main.js
@@ -1,60 +1,8 @@
-const navBar = document.querySelector("nav")
-
-const navLinks = [
-    {
-        id: "home-link",
-        element: document.getElementById("home-link"),
-        target: document.querySelector("header"),
-    },
-    {
-        id: "about-link",
-        element: document.getElementById("about-link"),
-        target: document.getElementById("about-section"),
-    },
-    {
-        id: "portfolio-link",
-        element: document.getElementById("portfolio-link"),
-        target: document.getElementById("portfolio-section"),
-    },
-]
-
-for (const link of navLinks) {
-    link.element.addEventListener("click", () => {
-        const bounds = link.target.getBoundingClientRect()
-        window.scrollTo(0, bounds.top)
-    })
-}
-
-/**
- * Checks whether or not an HTML element is currently visible
- * @param {HTMLElement} element
- * @returns whether or not element is visible
- */
-const isInView = element => {
-    const bounds = element.getBoundingClientRect()
-    return bounds.top >= 0 && bounds.top <= window.innerHeight
-}
-
-/**
- * Fades a element in or out based on where the element is on the screen.
- * @param {HTMLElement} element
- */
-const fadeElement = element => {
-    if (isInView(element)) {
-        element.classList.add("in-view")
+document.addEventListener("scroll", e => {
+    const navBar = document.querySelector("nav");
+    if (window.scrollY > 20) {
+        navBar.classList.remove("hide")
     } else {
-        element.classList.remove("in-view")
+        navBar.classList.add("hide");
     }
-}
-
-/**
- * Handle a scroll event
- * @param {Event} event
- */
-const handleScroll = event => {
-    for (const section of document.querySelectorAll("section")) {
-        fadeElement(section)
-    }
-}
-
-document.addEventListener("scroll", handleScroll)
+})
